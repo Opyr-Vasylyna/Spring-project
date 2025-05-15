bc3bbe025728987491c879834facb4d686b56411
opyr <vasylyna.opyr@gmail.com>
2025-05-15 19:40:08 +0300

    Add main Spring Boot application class

---
+++ src/test/java/ua/opyr/springproj/SpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.opyr.springproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
